c8eaf7a709650b012dc5e8ccb76714e536f3cb11
Martin Kavalar <martin@nextjournal.com>
2023-01-19 09:11:58 +0100

    Remove wrongly set fill-column from .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((clojure-mode . ((cider-clojure-cli-aliases . ":dev")))
- (text-mode . ((fill-column . -1))))
+((clojure-mode . ((cider-clojure-cli-aliases . ":dev"))))
